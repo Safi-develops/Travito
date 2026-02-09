09841d22539102f9490565e5eed2378714790fda
Safi-develops <safidevelops@gmail.com>
2026-02-09 19:00:21 +0430

    testing

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="bg-black">
+      <h1 className="text-white text-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quaerat
+        dolorum illo non maxime exercitationem eos maiores magnam eaque
+        asperiores. Maxime ipsum perferendis consequuntur labore voluptas
+        aperiam nobis id molestias!
+      </h1>
+    </div>
+  );
+}
+
+export default Hero;
